927cd720207b27aae2112b8977393ccbf842a2f8
Kush Chauhan <kushchauhan2004@gmail.com>
2025-10-31 02:11:56 +0530

    connected backend route to frontend

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -26,7 +26,7 @@ const GoogleSkillsBoostLeaderboard = () => {
       "linear-gradient(135deg, #ff6e7f 0%, #bfe9ff 100%)"
     ];
 
-    fetch('/api/students')
+    fetch('https://skillboost-backend-65uv.onrender.com/api/students')
       .then(res => {
         if (!res.ok) {
           throw new Error('Network response was not ok');
